caf65ee9b1ab17adc46883ee549e4045147b5ad1
Gosia <mrowka970@gmail.comm>
2018-07-10 21:23:01 +0200

    test

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 print("Hello Gosia")
 # TODO Lazy
+
+print("Podaj a:")
+a = input()
+print("Podaj b:")
+b = input()
+print("Podaj c:")
+c = input()
+
+float
+delta
+delta = (b ** (2)) - (4 * a * c)
+x1 = ((-b) - delta ** (1 / 2)) / (2 * a)
+x2 = ((-b) + delta ** (1 / 2)) / (2 * a)
