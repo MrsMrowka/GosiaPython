82f8ff10fd2a52ed0ef1fd86d110dce611b69b45
Gosia <mrowka970@gmail.comm>
2018-07-11 22:29:49 +0200

    coś innego

--- main.py
+++ main.py
@@ -1,2 +1,36 @@
 print("Hello Gosia")
 # TODO Lazy
+
+print("Wybierz działanie; 1. dodawanie 2. odejmowanie 3. mnożenie 4. dzielenie 5. potęgowanie 6. pierwiastkowanie")
+
+if int(input()) == 1:
+    print("podaj pierwszą liczbę:")
+    a = int(input())
+    print("podaj drugą liczbę:")
+    b = int(input())
+    c = a + b
+    print(c)
+
+elif int(input()) == 2:
+    print("podaj pierwszą liczbę:")
+    a = int(input())
+    print("podaj drugą liczbę:")
+    b = int(input())
+    c = a - b
+    print(c)
+
+elif int(input()) == 3:
+    print("podaj pierwszą liczbę:")
+    a = int(input())
+    print("podaj drugą liczbę:")
+    b = int(input())
+    c = a * b
+    print(c)
+
+elif int(input()) == 4:
+    print("podaj pierwszą liczbę:")
+    a = int(input())
+    print("podaj drugą liczbę:")
+    b = int(input())
+    c = a / b
+    print(c)
